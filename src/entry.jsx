ede5d651d8bf6c823c3c4ae12c3d1cbd65d471f5
Daniel Bucket <danieljbucket@gmail.com>
2025-08-01 07:53:09 -0600

    adjustment to css and an import improperly cased

--- src/entry.jsx
+++ src/entry.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Routes from './router.jsx';
 import { GlobalStyle } from './global.style.js';
-import AuthProvider from './providers/AuthProvider.jsx';
+import AuthProvider from './providers/authProvider.jsx';
 
 const root = document.getElementById('root');
 

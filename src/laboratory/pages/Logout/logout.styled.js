e3843c0892b761c5bf1a78d28adb97c2edf5744d
Daniel Bucket <danieljbucket@gmail.com>
2025-08-27 17:30:58 -0600

    logout and redirect back to login screen

---
+++ src/laboratory/pages/Logout/logout.styled.js
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+export const LogoutButton = styled.button`
+  background-color: #ff4d4d;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #ff1a1a;
+  }
+`;
